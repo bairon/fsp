9559041a7ead8b62799412e343499be53a88fac6
alsa <alexander.sokolovsky.a@gmail.com>
2016-11-03 12:16:02 +0300

    Project structure

---
+++ src/main/java/com/alsa/FspApplication.java
@@ -0,0 +1,12 @@
+package com.alsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FspApplication.class, args);
+	}
+}
